64230f853f830aee7b59f1162f7bb17012f888cf
Alex Grevtsev <aleksandr.grevtsev97@gmail.com>
2022-08-13 17:32:48 +1000

    add form for creating articles

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    use HasFactory;
+	
+	use HasFactory;
+	protected $fillable = ['name', 'body'];
 }
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,11 @@ use App\Http\Controllers\ArticleController;
 
 Route::get('/', [PageController::class, 'about']);
 
-Route::get('/articles', [ArticleController::class, 'index'])->name('acticles.index');
+Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
+
+Route::get('articles/create', [ArticleController::class, 'create']) ->name('articles.create');
 
 Route::get('/articles/{id}', [ArticleController::class, 'show'])->name('articles.show');
 
+Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
 
