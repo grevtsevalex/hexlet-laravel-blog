d829afdfa8a21bdf2e81ad894d54011e36212ede
Alex Grevtsev <aleksandr.grevtsev97@gmail.com>
2022-08-16 19:55:50 +1000

    make routes with artisan;

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,63 +2,110 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Article;
+use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-	public function index() {
-		$articles =  Article::paginate();
-		return view('article.index', compact('articles'));
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articles =  Article::paginate();
+	return view('article.index', compact('articles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+	$article = new Article();
+	return view('article.create', compact('article'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $this->validate($request, [
+		'name' => 'required|unique:articles',
+		'body' => 'required|min:10',
+	]);
+
+	$article = new Article();
+	$article->fill($data);
+	$article->save();
+
+	return redirect()->route('articles.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Article $article)
+    {
+   	$article = Article::findOrFail($article->id);
+	return view('article.show', compact('article'));  
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Article $article)
+    {
+        $article = Article::findOrFail($article->id);
+	return view('article.edit', compact('article'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Article $article)
+    {
+       	$article = Article::findOrFail($article->id);
+	$data = $this->validate($request, [
+		'name' => 'required|unique:articles, name,' . $article->id,
+		'body' => 'required|min: 10',
+	]);
+
+	$article->fill($data);
+	$article->save();
+	return redirect()->route('articles.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Article $article)
+    {
+       	$article = Article::find($article->id);
+	if ($article) {
+		$article->delete();
 	}
 
-	public function create() {
-		$article = new Article();
-		return view('article.create', compact('article'));
-	}
-
-	public function store(Request $request) {
-		$data = $this->validate($request, [
-			'name' => 'required|unique:articles',
-			'body' => 'required|min:10',
-		]);
-
-		$article = new Article();
-		$article->fill($data);
-		$article->save();
-
-		return redirect()->route('articles.index');
-	} 
-
-	public function show($id) {
-		$article = Article::findOrFail($id);
-		return view('article.show', compact('article'));
-	}
-	
-	public function edit($id) {
-		$article = Article::findOrFail($id);
-		return view('article.edit', compact('article'));
-	}	
-
-	public function update(Request $request, $id) {
-		$article = Article::findOrFail($id);
-		$data = $this->validate($request, [
-			'name' => 'required|unique:articles, name,' . $article->id,
-			'body' => 'required|min: 10',
-		]);
-
-		$article->fill($data);
-		$article->save();
-
-		return redirect()->route('articles.index');
-	}
-
-	public function delete($id) {
-		$article = Article::find($id);
-		if ($article) {
-			$article->delete();
-		}
-
-		return redirect()->route('articles.index');
-	}
+	return redirect()->route('articles.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,4 @@ use App\Http\Controllers\ArticleController;
 
 Route::get('/', [PageController::class, 'about']);
 
-Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
-
-Route::get('articles/create', [ArticleController::class, 'create']) ->name('articles.create');
-
-Route::get('/articles/{id}', [ArticleController::class, 'show'])->name('articles.show');
-
-Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
-
-Route::get('/articles/{id}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
-
-Route::delete('/articles/{id}', [ArticleController::class, 'delete'])->name('articles.delete');
-
-Route::patch('/articles/{id}', [ArticleController::class, 'update'])->name('articles.update');
+Route::resource('articles', ArticleController::class);
